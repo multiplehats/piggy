de774a53c88509ce98fd24be0658391f2b730b89
Jaeden <ch.schwartze@gmail.com>
2024-11-30 16:52:34 +0100

    chore: remove comment

--- apps/plugin/bin/copy-plugin-files.sh
+++ apps/plugin/bin/copy-plugin-files.sh
@@ -3,7 +3,6 @@ rsync ./ "$2"/ --recursive --delete --delete-excluded \
 	--exclude=".*/" \
 	--exclude="*.md" \
 	--exclude=".*" \
-	## Do not exclude composer.json as it's required by WordPress guideliens.
 	--exclude="composer.lock" \
 	--exclude="*.lock" \
 	--exclude=bin/ \
